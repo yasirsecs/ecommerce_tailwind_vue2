42205009f9cdfd58b7de6c7d952c80b4d2e45210
yasirsecs <114733212+yasirsecs@users.noreply.github.com>
2022-10-01 15:01:39 +0500

    lcp3 into ecommerce

--- routes/web.php
+++ routes/web.php
@@ -2,40 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/login', function () {
-    return view('login');
+
+
+Route::get('/yasir', function () {
+    return view('yasir');
 });
 
-Route::get('/pcard', function () {
-    return view('pcard');
-});
-Route::get('/amir', function () {
-    return view('amir');
-});
-
-Route::get('/pcard2', function () {
-    return view('pcard2');
-});
-Route::get('/pcard3', function () {
-    return view('pcard3');
-});
-Route::get('/pcard4', function () {
-    return view('pcard4');
-});
-Route::get('/pcard5', function () {
-    return view('pcard5');
-});
-Route::get('/product', function () {
-    return view('landing-product');
-});
 Route::get('/landing', function () {
     return view('landing-front');
 });
 
-Route::get('/amirlanding', function () {
-    return view('amirlanding');
-});
